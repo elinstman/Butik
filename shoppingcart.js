8ffc60b8b7858320600a33395293519da8d555d3
Elin Östman <elin.ostman@hotmail.com>
2023-10-17 19:48:02 +0200

    skapat klass

--- shoppingcart.js
+++ shoppingcart.js
@@ -18,6 +18,64 @@ const ShoppingCartModule = (function () {
   };
 })();
 
+class Cart {
+  constructor() {
+    this.items = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+  }
+  // Funktion för att lägga till en produkt i varukorgen
+  addToCart(product) {
+    const existingProduct = this.items.find(
+      (item) => item.title === product.title
+    );
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      product.quantity = 1;
+      this.items.push(product);
+    }
+    this.updateCart();
+    localStorage.setItem("cart", JSON.stringify(this.items));
+  }
+
+  getTotalCartQuantity() {
+    return this.items.reduce((total, product) => total + product.quantity, 0);
+  }
+
+  removeProduct(productIndex) {
+    this.items.splice(productIndex, 1);
+    localStorage.setItem("cart", JSON.stringify(this.items));
+    // Uppdatera varukorgsgränssnittet
+    this.updateCart();
+  }
+
+  // Funktion för att uppdatera varukorgen
+  updateCart() {
+    const cartItems = document.getElementById("cart-items");
+    cartItems.innerHTML = ""; // Rensa varukorgens innehåll
+
+    this.items.forEach(function (product) {
+      const cartItem = document.createElement("div");
+      cartItem.className = "cart-item-style";
+      cartItem.innerHTML = `
+        <img src="${product.image}" alt="${product.title}">
+        <div class="cart-item-details">
+          <h4>${product.title}</h4>
+          <p>Pris: ${product.price}</p>
+          <p>Antal: ${product.quantity}</p>
+        </div>
+        <div class="cart-item-actions">
+          <button class="btn btn-outline-secondary btn-sm remove">Ta bort</button>
+        </div>
+      `;
+      cartItems.appendChild(cartItem);
+    });
+    const cartLink = document.getElementById("cart-link");
+    cartLink.textContent = `(${this.getTotalCartQuantity()})`;
+  }
+}
+
 // Visa shopping cart modal när användaren klickar på varukorgssymbolen
 const cartIcon = document.getElementById("cart-icon");
 const shoppingCartModal = document.getElementById("shopping-cart");
@@ -35,46 +93,7 @@ closeCart.addEventListener("click", function () {
 const cartItems = document.getElementById("cart-items");
 
 // Produkter i varukorgen
-let cart = [];
-
-if (localStorage.getItem("cart")) {
-  cart = JSON.parse(localStorage.getItem("cart"));
-}
-
-// Funktion för att uppdatera varukorgen
-function updateCart() {
-  cartItems.innerHTML = ""; // Rensa varukorgens innehåll
-
-  cart.forEach(function (product) {
-    const cartItem = document.createElement("div");
-    cartItem.className = "cart-item-style";
-    cartItem.innerHTML = `
-        <img src="${product.image}" alt="${product.title}">
-        <div class="cart-item-details">
-          <h4>${product.title}</h4>
-          <p>Pris: ${product.price}</p>
-          <p>Antal: ${product.quantity}</p>
-        </div>
-        <div class="cart-item-actions">
-          <button class="btn btn-outline-secondary btn-sm remove">Ta bort</button>
-        </div>
-      `;
-    cartItems.appendChild(cartItem);
-  });
-}
-
-// Funktion för att lägga till en produkt i varukorgen
-function addToCart(product) {
-  const existingProduct = cart.find((item) => item.title === product.title);
-  if (existingProduct) {
-    existingProduct.quantity += 1;
-  } else {
-    product.quantity = 1;
-    cart.push(product);
-  }
-  updateCart();
-  localStorage.setItem("cart", JSON.stringify(cart));
-}
+let cart = new Cart();
 
 // "Lägg till i varukorgen"
 document.getElementById("add-to-cart").addEventListener("click", function () {
@@ -88,16 +107,8 @@ document.getElementById("add-to-cart").addEventListener("click", function () {
     image: productImage,
   };
 
-  addToCart(product);
-
-  // Uppdatera varukorgsräknaren i headern
-  const cartLink = document.getElementById("cart-link");
-  cartLink.textContent = `(${getTotalCartQuantity()})`;
+  cart.addToCart(product);
 });
-
-function getTotalCartQuantity() {
-  return cart.reduce((total, product) => total + product.quantity, 0);
-}
 
 // klick för "Ta bort" -knappen
 document
@@ -111,24 +122,10 @@ document
 
       // letar upp och tar bort produkten
       if (productIndex >= 0) {
-        const removedProduct = cart.splice(productIndex, 1)[0];
-        // Uppdatera varukorgsgränssnittet
-        updateCart();
-        const cartLink = document.getElementById("cart-link");
-        cartLink.textContent = `(${getTotalCartQuantity()})`;
-
-        removeFromLocalStorage(removedProduct);
+        cart.removeProduct(productIndex);
       }
     }
   });
 
-function removeFromLocalStorage(product) {
-  const existingCart = JSON.parse(localStorage.getItem("cart")) || [];
-  const updatedCart = existingCart.filter(
-    (item) => item.title !== product.title
-  );
-  localStorage.setItem("cart", JSON.stringify(updatedCart));
-}
-
 // Initiera varukorgen
-updateCart();
+cart.updateCart();
